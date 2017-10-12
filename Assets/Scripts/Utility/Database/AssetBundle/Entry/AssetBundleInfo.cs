398550a89f0d441082a397aaf400cb4e359ad2cc
Riley <simplemangame@gmail.com>
2017-10-11 21:55:07 -0700

    Started work on the asset bundle database Created the xml for the asset bundle database Renamed the database that represented items to a better name

---
+++ Assets/Scripts/Utility/Database/AssetBundle/Entry/AssetBundleInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+[XmlRoot("AssetBundleInfo")]
+public sealed class AssetBundleInfo : DatabaseEntry
+{
+    [XmlElement]
+    public string Name { get; set; }
+
+    [XmlElement]
+    public string URL { get; set; }
+}
---
+++ Assets/Scripts/Utility/Database/AssetBundle/Entry/AssetInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+[XmlRoot("AssetInfo")]
+public sealed class AssetInfo : DatabaseEntry
+{
+    [XmlElement]
+    public string Path { get; set; }
+
+    [XmlElement]
+    public DatabaseEntryRef<AssetBundleInfo> AssetBundleInfoRef { get; set; }
+}
