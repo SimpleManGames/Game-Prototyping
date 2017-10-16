bb11a5755009a23c3a596a8b0eb08138ed773abb
Riley <simplemangame@gmail.com>
2017-10-15 22:01:00 -0700

    Added more tags to the network transfer class

--- Assets/Scripts/Network/Helper/NetworkConst.cs
+++ Assets/Scripts/Network/Helper/NetworkConst.cs
@@ -10,6 +10,12 @@ namespace Core.Network
             public const ushort loginUser = 1;
             public const ushort logoutUser = 2;
             public const ushort addUser = 3;
+
+            public const ushort loginUserSuccess = 4;
+            public const ushort loginUserFailed = 5;
+            public const ushort logoutSuccess = 6;
+            public const ushort addUserSuccess = 7;
+            public const ushort addUserFailed = 8;
         }
     }
 }
