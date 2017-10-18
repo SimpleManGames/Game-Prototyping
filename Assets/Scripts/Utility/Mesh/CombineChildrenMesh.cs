89bcb57faa44e0e5e37b51d839e48cbbeb67ec09
Riley <simplemangame@gmail.com>
2017-10-17 20:21:57 -0700

    Added a mesh combiner and a component to run on awake to combine children meshes to the parent Added better walking animation for the mech

---
+++ Assets/Scripts/Utility/Mesh/CombineChildrenMesh.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SkinnedMeshRenderer))]
+public class CombineChildrenMesh : MonoBehaviour
+{
+    private void Awake()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Combiner.Combine(gameObject, transform.GetChild(i).gameObject);
+            Destroy(transform.GetChild(i));
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/Mesh/Combiner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combiner
+{
+    //
+    // Summary:
+    //      Takes in a source to add to the target
+    public static GameObject Combine(GameObject target, GameObject source)
+    {
+        Dictionary<string, Transform> catalog = new Dictionary<string, Transform>() { { target.name, target.transform } };
+        SkinnedMeshRenderer[] meshes = source.GetComponentsInChildren<SkinnedMeshRenderer>();
+        GameObject targetMesh = AddChild(source, target.transform);
+
+        foreach (SkinnedMeshRenderer sourceRenderer in meshes)
+        {
+            SkinnedMeshRenderer targetRenderer = targetMesh.GetComponent<SkinnedMeshRenderer>();
+            targetRenderer.sharedMesh = sourceRenderer.sharedMesh;
+            targetRenderer.materials = sourceRenderer.materials;
+
+            targetRenderer.bones = TranslateTransforms(sourceRenderer.bones, catalog);
+        }
+
+        return target;
+    }
+
+    private static GameObject AddChild(GameObject source, Transform transform)
+    {
+        GameObject target = new GameObject(source.name);
+        target.transform.parent = transform;
+
+        target.transform.localPosition = source.transform.localPosition;
+        target.transform.localRotation = source.transform.localRotation;
+        target.transform.localScale = source.transform.localScale;
+
+        return target;
+    }
+
+    private static Transform[] TranslateTransforms(Transform[] sources, Dictionary<string, Transform> transformCatalog)
+    {
+        Transform[] targets = new Transform[sources.Length];
+        for (int index = 0; index < sources.Length; index++)
+            transformCatalog.TryGetValue(sources[index].name, out targets[index]);
+
+        return targets;
+    }
+}
